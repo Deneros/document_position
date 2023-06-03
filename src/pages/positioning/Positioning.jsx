610d0ae28a583fb1b82858353d5de65aa97b3ac8
Nicolas <nicolas.velez99@hotmail.com>
2023-06-03 02:04:33 -0500

    [feat]: se recoge la posicion de cada uno

--- src/pages/positioning/Positioning.jsx
+++ src/pages/positioning/Positioning.jsx
@@ -1,78 +1,57 @@
+// import { PDFDocument } from 'pdf-lib';
 import { useState, useEffect } from 'react';
-import { PDFDocument } from 'pdf-lib';
 import Sidebar from "../../components/Sidebar/Sidebar";
 import PdfViewer from "../../components/Canvas/Canvas";
 import SignatureComponent from "../../components/Draggable/SignaturePositioning";
+// import {useFetch} from '../../hooks/useFetch';
+
 import pdf from '../../assets/document/testing.pdf';
 import img from '../../assets/img/grafofirma.png';
 
 
-async function createPdf(insertImage) {
-    // Fetch existing PDF
-    const existingPdfBytes = await fetch(pdf).then(res => res.arrayBuffer())
-
-    // Load a PDFDocument from the existing PDF bytes
-    const pdfDoc = await PDFDocument.load(existingPdfBytes, { ignoreEncryption: true })
-
-    // Fetch image
-    const imgBytes = await fetch(insertImage.src).then(res => res.arrayBuffer())
-
-    // Embed the PNG image bytes
-    const pngImage = await pdfDoc.embedPng(imgBytes)
-
-    // Get the first page (zero indexed)
-    const pages = pdfDoc.getPages()
-    const firstPage = pages[0]
-
-    // Draw the image on the first page of the PDF
-    firstPage.drawImage(pngImage, {
-        x: insertImage.x,
-        y: insertImage.y,
-        width: pngImage.width,
-        height: pngImage.height
-    })
-
-    // Serialize the PDFDocument to bytes (a Uint8Array)
-    const pdfBytes = await pdfDoc.save()
-
-    // Create a Blob from the PDF bytes
-    const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-
-    // Create a URL from the Blob
-    const url = URL.createObjectURL(blob);
-
-    // Display the PDF in an iframe
-    const iframe = document.createElement('iframe');
-    iframe.src = url;
-    document.body.appendChild(iframe);
-}
 
 export default function Positioning() {
-    const [pdfPosition, setPdfPosition] = useState({ x: 0, y: 0 });
+    const data = [{ name: 'Nico chulo' }, { name: 'Papi Pepa' }];
+    const [draggedItems, setDraggedItems] = useState([]);
     const [insertImage, setInsertImage] = useState(null);
+    // const [pdf, setPdf] = useState(null);
 
-    const handleStop = (event, data) => {
-        setInsertImage({
-            signImg: img,
-            x: pdfPosition.x - data.x,
-            y: pdfPosition.y - data.y
-        })
-        console.log(`Elemento soltado en x: ${pdfPosition.x - data.x}, y: ${pdfPosition.y - data.y}`);
-        // createPdf(insertImage);
+
+
+    const handleStop = (event, data, name) => {
+        // Obtén la posición del elemento que contiene tu PDF
+        const pdfContainer = document.getElementById('pdfContainer');
+        const rect = pdfContainer.getBoundingClientRect();
+
+        // Calcula la posición del arrastrable en relación con el elemento que contiene el PDF
+        const x = data.x - rect.left;
+        const y = data.y - rect.top;
+
+        setDraggedItems(prevState => [...prevState, { name, position: { x, y } }]);
+
+        // console.log(`Elemento soltado en x: ${x}, y: ${y}`);
     };
 
     useEffect(() => {
-        if (insertImage) {
-            // createPdf(insertImage);
-        }
-    }, [insertImage]);
+        console.log(draggedItems);
+    }, [draggedItems])
+    
 
     return (
         <>
             <div>
-                <Sidebar draggableComponents={[SignatureComponent]} handleStop={handleStop} />
+                <Sidebar
+                    draggableComponents={
+                        data.map((destinatary) => ({
+                            component: SignatureComponent,
+                            props: { name: destinatary.name },
+                            handleStop: (event, data) => handleStop(event, data, destinatary.name)
+                        }))
+                    }
+                />
             </div>
-            <PdfViewer path={pdf} setPdfPosition={setPdfPosition} insertImage={insertImage}></PdfViewer>
+            <PdfViewer path={pdf} insertImage={insertImage}></PdfViewer>
         </>
     )
 }
+
