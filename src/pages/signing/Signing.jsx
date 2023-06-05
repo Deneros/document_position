45471c4debdc6b4a0e3f88e98be0133ac36f4983
Nicolas <nicolas.velez99@hotmail.com>
2023-06-05 01:27:56 -0500

    [feat]: firma multiple

--- src/pages/signing/Signing.jsx
+++ src/pages/signing/Signing.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import PdfViewer from "../../components/Canvas/Canvas";
 import DocumentSigning from "../../components/DocumentSigning/DocumentSigning";
+import { useParams } from 'react-router-dom';
 import pdf from '../../assets/document/testing.pdf';
+// import img from '../../assets/img/grafofirma.png';
+
 
 const PdfContainer = styled.div`
   position: relative;
@@ -10,22 +14,39 @@ const PdfContainer = styled.div`
 
 const signers = [
     {
-        name: "Firmante 1",
+        id:2412314,
+        name: "Nicolas Velez",
         x: 200,
-        y: 300
+        y: 300,
+        signImg:""
     },
     {
-        name: "Firmante 2",
+        id:3439283,
+        name: "Sachat Quintero",
         x: 500,
-        y: 700
+        y: 700,
+        signImg:""
     }
 ];
 
 export default function Signing() {
+    const [insertImage, setInsertImage] = useState([]);
+    const { id } = useParams();
+
+    const insertSignature = (image, x, y) => {
+        setInsertImage(prevImages => {
+            if (!Array.isArray(prevImages)) {
+                console.error('prevImages is not an array:', prevImages);
+                return [{ signImg: image, x, y }];
+            }
+            return [...prevImages, { signImg: image, x, y }];
+        });
+    };
+
     return (
         <PdfContainer>
-            <PdfViewer path={pdf} />
-            <DocumentSigning signers={signers} />
+            <PdfViewer path={pdf} insertImage={insertImage} />
+            <DocumentSigning signers={signers} insertSignature={insertSignature} signerId={Number(id)}/>
         </PdfContainer>
     );
 }
